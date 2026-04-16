3a3a4be5c9f9978064b56751515542586bd92de0
abdalrhman amr <abdalrhmanamr14@gmail.com>
2026-04-16 18:53:44 +0200

    Change app name

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Design Patterns Flutter',
+      title: 'Design Patterns Explorer',
       home: const HomePage(),
     );
   }
